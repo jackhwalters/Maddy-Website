70405719b042d628fc292072b0ccda98b6446760
Jack Walters <jackwalterswork@gmail.com>
2023-05-21 17:48:08 +0100

    Populate about and contact pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import '../css/About.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const About = () => {
     return (
-        <div>The About component!</div>
+        <div className="about-container">
+            <Container fluid>
+                <Row>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam rhoncus aliquam mauris, sit amet egestas quam rutrum nec.
+                        Suspendisse potenti. Sed volutpat, neque non efficitur lacinia,
+                        turpis nisi vulputate ex, a fringilla turpis enim vel est.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam rhoncus aliquam mauris, sit amet egestas quam rutrum nec.
+                        Suspendisse potenti. Sed volutpat, neque non efficitur lacinia,
+                        turpis nisi vulputate ex, a fringilla turpis enim vel est.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam rhoncus aliquam mauris, sit amet egestas quam rutrum nec.
+                        Suspendisse potenti. Sed volutpat, neque non efficitur lacinia,
+                        turpis nisi vulputate ex, a fringilla turpis enim vel est.
+                    </p>
+                </Row>
+                <Row>
+                    <img className="img-fluid about-image" src="images/maddy.jpg" alt="About Image" />
+                </Row>
+            </Container>
+        </div>
     )
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import '../css/Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faTiktok, faImdb } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     return (
-        <div>The Contact component!</div>
+        <div className="contact-container">
+            <div className="contact-container-text">
+                <p>
+                    Please reach out through any of the following channels. I'd love to work together!
+                </p>
+            </div>
+            <div className="contact-btns">
+                <div className="contact-btn">
+                    <a href="mailto: hello@maddywalters.com">
+                        <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                    </a>
+                </div>
+                <div className="contact-btn">
+                    <a href="https://www.instagram.com/maddywfilm/" target="_blank">
+                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                    </a>
+                </div>
+                <div className="contact-btn">
+                    <a href="https://www.tiktok.com/" target="_blank">
+                        <FontAwesomeIcon icon={faTiktok} size="2x" />
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
 
